119645196a969bfa1fb60b45cb7402b42c902922
Ttales R G S Silva <ttales@MacBook-Air-de-Ttales.local>
2019-04-08 15:57:06 -0300

    conf db

--- env.php
+++ env.php
@@ -1,3 +1,9 @@
 <?php
 
     putenv('DISPLAY_ERRORS_DETAILS='. true);
+
+    putenv('DB_API_HOST=localhost');
+    putenv('DB_API_DBNAME=db_api');
+    putenv('DB_API_USER=root');
+    putenv('DB_API_PASSWORD=');
+    putenv('DB_API_PORT=3306');
