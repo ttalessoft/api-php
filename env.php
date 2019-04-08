43ca16ad876cee9249df065893a56fa5e30bb340
ttales r g s silva <ttalessoft@gmail.com>
2019-04-08 15:57:42 -0300

    Update env.php

--- env.php
+++ env.php
@@ -5,5 +5,5 @@
     putenv('DB_API_HOST=localhost');
     putenv('DB_API_DBNAME=db_api');
     putenv('DB_API_USER=root');
-    putenv('DB_API_PASSWORD=');
+    putenv('DB_API_PASSWORD=root');
     putenv('DB_API_PORT=3306');
